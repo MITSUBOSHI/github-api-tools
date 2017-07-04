9f7cda9ee916a42048bf78219ca579a0d09eeb0e
mitsubosh <yuya.mitsuboshi@gmail.com>
2017-07-04 23:27:02 +0900

    First Commit

---
+++ mkrepo-org-private.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ $# -ne 4 ]; then
+  echo "[usage]" 1>&2
+  echo 'mkrepo-public.sh {token} {repo_name} {repo_desc} {ORG}' 1>&2
+  exit 1
+fi
+
+
+TOKEN=$1
+JSON="{\"name\": \"$2\", \"description\": \"$3\", \"private\": true}"
+
+curl -i -H "Authorization: token ${TOKEN}" -d "${JSON}" https://api.github.com/orgs/${ORG}/repos;
---
+++ mkrepo-public.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ $# -ne 3 ]; then
+  echo "[usage]" 1>&2
+  echo 'mkrepo-public.sh {token} {repo_name} {repo_desc}' 1>&2
+  exit 1
+fi
+
+
+TOKEN=$1
+JSON="{\"name\": \"$2\", \"description\": \"$3\"}"
+
+curl -i -H "Authorization: token ${TOKEN}" -d "${JSON}" https://api.github.com/user/repos;
